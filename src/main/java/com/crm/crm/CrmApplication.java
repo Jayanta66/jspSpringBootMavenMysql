91bc203bd070bc1585da68f824fe1d88162d134e
Jayanta66 <jayhalder18@gmail.com>
2024-07-30 19:26:02 +0530

    JSP with jstl, tomcat-embed-jasper,mysql,jpa on Maven

---
+++ src/main/java/com/crm/crm/CrmApplication.java
@@ -0,0 +1,16 @@
+package com.crm.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+	
+
+}
